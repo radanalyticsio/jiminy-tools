ca873c5d2fe3d10d8727814ba2a8d5f258b3994d
Michael McCune <msm@redhat.com>
2017-11-28 14:15:28 -0500

    fix predictor mock hostname for openshift
    it was set to localhost, now set to 0.0.0.0

--- predictor-mock/app.py
+++ predictor-mock/app.py
@@ -142,4 +142,4 @@ if __name__ == '__main__':
     api.add_resource(RatingsPredictionResponse, '/predictions/ratings/<p_id>')
     api.add_resource(RankingsPredictions, '/predictions/ranks')
     api.add_resource(RankingsPredictionResponse, '/predictions/ranks/<p_id>')
-    app.run(debug=True, port=8080)
+    app.run(debug=True, host="0.0.0.0", port=8080)
