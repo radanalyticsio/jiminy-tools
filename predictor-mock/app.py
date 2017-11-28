6545ec920e67a190701206943b6889587b2a3a26
Michael McCune <msm@redhat.com>
2017-11-28 13:53:25 -0500

    add rankings endpoints to predictor mock

--- predictor-mock/app.py
+++ predictor-mock/app.py
@@ -8,6 +8,7 @@ app = flask.Flask(__name__)
 api = flaskr.Api(app)
 
 ratings_store = {}
+rankings_store = {}
 
 class ServerInfo(flaskr.Resource):
     def get(self):
@@ -31,6 +32,62 @@ rating_request_schema = {
         },
     "required": ["user", "products"]
     }
+
+
+rank_request_schema = {
+    "type": "object",
+    "properties": {
+        "user": {"type": "string"},
+        "topk": {"type": "integer"},
+        },
+    "required": ["user", "topk"]
+    }
+
+
+class RankingsPredictions(flaskr.Resource):
+    def post(self):
+        data = flask.request.json
+        try:
+            js.validate(data, rank_request_schema)
+            resp = {
+                    "id": str(len(rankings_store) + 1),
+                    "user": data["user"],
+                    "topk": data["topk"],
+                    "products": []
+                }
+            rankings_store[resp["id"]] = {
+                    "id": resp["id"],
+                    "user": resp["user"],
+                    "products": [ {"id": i+1, "rating": 5.0}
+                                for i in range(resp["topk"])]
+                }
+            return resp, 201
+        except js.ValidationError as ex:
+            return {
+                    "errors": [
+                        {
+                            "status": 400,
+                            "title": "invalid request format",
+                            "details": ex.message
+                            }
+                        ]
+                    }, 400
+
+
+class RankingsPredictionResponse(flaskr.Resource):
+    def get(self, p_id):
+        if str(p_id) not in rankings_store:
+            return {
+                    "errors": [
+                        {
+                            "status": 404,
+                            "title": "not found",
+                            "details": "the requested ranking id {} "
+                            "cannot be found".format(p_id)
+                            }
+                        ]
+                }, 404
+        return rankings_store[p_id]
 
 
 class RatingsPredictions(flaskr.Resource):
@@ -70,7 +127,7 @@ class RatingsPredictionResponse(flaskr.Resource):
                         {
                             "status": 404,
                             "title": "not found",
-                            "details": "the requested prediction id {} "
+                            "details": "the requested ratings id {} "
                             "cannot be found".format(p_id)
                             }
                         ]
@@ -83,4 +140,6 @@ if __name__ == '__main__':
     api.add_resource(ServerInfo, '/')
     api.add_resource(RatingsPredictions, '/predictions/ratings')
     api.add_resource(RatingsPredictionResponse, '/predictions/ratings/<p_id>')
+    api.add_resource(RankingsPredictions, '/predictions/ranks')
+    api.add_resource(RankingsPredictionResponse, '/predictions/ranks/<p_id>')
     app.run(debug=True)
